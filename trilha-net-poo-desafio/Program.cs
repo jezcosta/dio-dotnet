20fc8226af5b5fb50f7764481afe28d50e939416
jezcosta <jessicacosta.dev@hotmail.com>
2024-08-16 00:54:00 -0300

    feat(net poo): adding challenge solution

--- trilha-net-poo-desafio/Program.cs
+++ trilha-net-poo-desafio/Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "1234567", modelo: "nokia1", imei: "1234356547", memoria: 2048);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+Smartphone iphone = new Iphone(numero: "7654321", modelo: "iphone1", imei: "7464516456", memoria: 4096);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
